1d3255650a205a6f86cc20f4db11c89d6e047ea0
pavan7297 <mallapavan072@gmail.com>
2026-01-05 10:16:42 +0530

    serverfile update

---
+++ servervices/gate/src/test/java/in/pavanoriginals/gate/GateApplicationTests.java
@@ -0,0 +1,13 @@
+package in.pavanoriginals.gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
